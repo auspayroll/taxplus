215ed2b10372307614ac5d4a70d6458e243f7ecf
simonagrant <simon@propertymode.com.au>
2014-05-20 17:07:55 +1000

    extended field length for name field in BusinessSubCategory model

--- asset/models.py
+++ asset/models.py
@@ -13,7 +13,7 @@ class BusinessCategory(models.Model):
 
 
 class BusinessSubCategory(models.Model):
-	name =  models.CharField(max_length=50)
+	name =  models.CharField(max_length=100)
 	business_category = models.ForeignKey(BusinessCategory, null=True, blank=True)
 
 
