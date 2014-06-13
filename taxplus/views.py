34b083d6dc81be17c269e05bc90943dbccdbe77c
simonagrant <simon@propertymode.com.au>
2014-06-13 15:09:38 +1000

    cleaning fee audit report: csv downloads

--- taxplus/views.py
+++ taxplus/views.py
@@ -6,18 +6,100 @@ from datetime import date
 import json
 from taxplus.forms import SearchForm
 from django.db.models import Q, Sum
+import csv
+from django.http import HttpResponse
 
 
 
+def cleaning_audit_csv(payments, includes):
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename="cleaning_fee_audit.csv"'
+	writer = csv.writer(response)
+	row = []
+	header = []
+	header.append('Payment Amount')
+	header.append('Month/Year')
 
+	if 'Business Name' in includes:
+		header.append('Business Name')
+
+	if 'Cell' in includes:
+		header.append('Cell')
+
+	if 'Fines' in includes:
+		header.append('Fines')
+
+	if 'Receipt' in includes:
+		header.append('Sector Receipt')
+
+	if 'Bank' in includes:
+		header.append('Bank')
+
+	if 'Bank Receipt' in includes:
+		header.append('Bank Receipt')
+
+	if 'User' in includes:
+		header.append('User')
+
+	if 'Timestamp' in includes:
+		header.append('Timestamp')
+
+	if 'Total Fee Amount' in includes:
+		header.append('Total Fee Amount')
+
+	if 'Remaining Fee Amount' in includes:
+		header.append('Remaining Amount')
+
+	writer.writerow(header)
+
+	for p in payments:
+		row.append(p.amount)
+		row.append(p.fee.date_from.strftime('%b/%Y'))
+
+		if 'Business Name' in includes:
+			if p.fee.subbusiness:
+				row.append(p.fee.subbusiness.name)
+			else:
+				row.append(p.fee.business.name)	
+		if 'Cell' in includes:
+			if p.fee.subbusiness and p.fee.subbusiness.business.cell:
+				row.append(p.fee.subbusiness.business.cell.name)
+			elif p.fee.business and p.fee.business.cell:
+				row.append(p.fee.business.cell.name)
+
+		if 'Fines' in includes:
+			row.append(p.fine_amount)
+
+		if 'Receipt' in includes:
+			row.append(p.manual_receipt)
+
+		if 'Bank' in includes:
+			row.append(p.bank)
+
+		if 'Bank Receipt' in includes:
+			row.append(p.receipt_no)
+
+		if 'User' in includes:
+			row.append(p.staff.username)
+
+		if 'Timestamp' in includes:
+			row.append(p.date_time)
+
+		if 'Total Fee Amount' in includes:
+			row.append(p.fee.amount)
+
+		if 'Remaining Fee Amount' in includes:
+			row.append(p.fee.remaining_amount)
+
+		writer.writerow(row)
+
+	return response
 
 
 def cleaning_audit(request):
 	user = request.session.get('user')
 	if not user or not user.superuser:
 		return HttpResponseRedirect('/')
-	date_from = date(2000,1,1)
-	date_to = date(2014,12,24)
 	payments = PayFee.objects.none()
 	totals = {}
 	include_fields = []
@@ -34,6 +116,16 @@ def cleaning_audit(request):
 			totals['fee'] = payments.aggregate(Sum('fee__amount'))['fee__amount__sum']
 			totals['remaining'] = payments.aggregate(Sum('fee__remaining_amount'))['fee__remaining_amount__sum']
 			totals['fines'] = payments.aggregate(Sum('fine_amount'))['fine_amount__sum']
+
+			if request.POST.get('web_button') or not payments:
+				return TemplateResponse(request, 'tax/cleaning_fee_audit.html', { 'payments':payments, 'form':form, 'include_fields':include_fields, 'totals':totals })
+			else: # csv
+				return cleaning_audit_csv(payments, form.cleaned_data.get('include_fields'))
+
 	else:
 		form = SearchForm()
-	return TemplateResponse(request, 'tax/cleaning_fee_audit.html', { 'payments':payments, 'form':form, 'include_fields':include_fields, 'totals':totals })
+
+	return TemplateResponse(request, 'tax/cleaning_fee_audit.html', { 'payments':None, 'form':form, })
+
+
+
