2d8891622c5cedfd2e5f1149b2fec52754e15139
simonagrant <simon@propertymode.com.au>
2014-06-19 10:26:43 +1000

    addition of cleaning fee debtors report

--- taxplus/forms.py
+++ taxplus/forms.py
@@ -41,3 +41,36 @@ class SearchForm(forms.Form):
 		cleaned_data = super(SearchForm, self).clean()
 		return cleaned_data
 
+
+overdue_choices = [('1','less than 1 month'), ('30', 'greater than 1 month'),('90','greater than 3 months'),('180','greater than 6 months'),('365','greater than 1 year')]
+
+class DebtorsForm(forms.Form):
+	district = forms.ModelChoiceField(queryset = District.objects.all(), error_messages={'required':'District is required'})
+	sector = forms.ModelChoiceField(queryset = Sector.objects.none(), error_messages={'required':'Sector is required'})
+	cell = forms.ModelChoiceField(required = False, queryset = Cell.objects.none())
+	village = forms.ModelChoiceField(required = False, queryset = Village.objects.none())
+	as_at = forms.DateField(input_formats=settings.DATE_INPUT_FORMATS, error_messages={'invalid':"'as at' date is invalid", 'required':"'as at' date is required"}, initial=date.today().strftime('%d/%m/%Y'), widget=forms.TextInput(attrs={'class':'date_picker'}))
+	include_fields = forms.MultipleChoiceField(required=False, widget=CheckboxSelectMultiple, choices=overdue_choices)
+
+	def __init__(self, *args, **kw):
+		super(DebtorsForm, self).__init__(*args, **kw)
+		self.fields['district'].queryset = District.objects.all()
+		self.fields['sector'].queryset = Sector.objects.all()
+		self.fields['cell'].queryset = Cell.objects.all()
+		self.fields['village'].queryset = Village.objects.all()
+		self.initial = {'include_fields':[ i for (i,j) in overdue_choices ]}
+
+	def clean(self):
+		cleaned_data = super(DebtorsForm, self).clean()
+		district = cleaned_data.get("district")
+		if district:
+			self.fields['sector'].queryset = Sector.objects.filter(district=district)
+
+		cleaned_data = super(DebtorsForm, self).clean()
+		sector = cleaned_data.get("sector")
+		if sector:
+			self.fields['cell'].queryset = Cell.objects.filter(sector=sector)
+
+		cleaned_data = super(DebtorsForm, self).clean()
+		return cleaned_data
+
--- taxplus/views.py
+++ taxplus/views.py
@@ -1,10 +1,10 @@
 from django.template.response import TemplateResponse 
 from django.shortcuts import HttpResponseRedirect
-from jtax.models import PayFee
+from jtax.models import PayFee, Fee
 from property.models import District, Sector, Cell
 from datetime import date
 import json
-from taxplus.forms import SearchForm
+from taxplus.forms import SearchForm, DebtorsForm
 from django.db.models import Q, Sum
 import csv
 from django.http import HttpResponse
@@ -132,3 +132,46 @@ def cleaning_audit(request):
 
 
 
+def cleaning_debtors(request):
+	user = request.session.get('user')
+	if not user or not user.superuser:
+		return HttpResponseRedirect('/')
+	fees = Fee.objects.none()
+	totals = {}
+	include_fields = []
+	if request.method == 'POST':
+		form = DebtorsForm(request.POST)
+		if form.is_valid():
+			include_fields = form.cleaned_data['include_fields']
+
+			as_at = form.cleaned_data['as_at']
+			fees = Fee.objects.filter(fee_type='cleaning', remaining_amount__gt=0, i_status='active', due_date__lt=form.cleaned_data['as_at']).select_related('business').order_by('date_time')
+			if form.cleaned_data['sector']:
+				fees = fees.filter(Q(business__sector=form.cleaned_data['sector'])| Q(subbusiness__sector=form.cleaned_data['sector']))
+			if form.cleaned_data['cell']:
+				fees = fees.filter(Q(business__cell=form.cleaned_data['cell']) | Q(subbusiness__cell=form.cleaned_data['cell']))
+			totals['amount'] = fees.aggregate(Sum('amount'))['amount__sum']
+			totals['remaining'] = fees.filter(remaining_amount__gte=0).aggregate(Sum('remaining_amount'))['remaining_amount__sum']
+
+			for fee in fees:
+				if (as_at - fee.due_date).days >= 365:
+					fee.late_year = fee.remaining_amount
+				elif (as_at - fee.due_date).days >= 120:
+					fee.late_half_year = fee.remaining_amount
+				elif (as_at - fee.due_date).days >= 90:
+					fee.late_quarter_year = fee.remaining_amount
+				elif (as_at - fee.due_date).days >= 30:
+					fee.late_month = fee.remaining_amount
+				else:
+					fee.late = fee.remaining_amount
+
+
+			if request.POST.get('web_button') or not fees:
+				return TemplateResponse(request, 'tax/cleaning_fee_debtors.html', { 'fees':fees, 'form':form, 'totals':totals, 'include_fields':include_fields })
+			else: # csv
+				return cleaning_audit_csv(fees, form.cleaned_data.get('include_fields'))
+
+	else:
+		form = DebtorsForm()
+
+	return TemplateResponse(request, 'tax/cleaning_fee_debtors.html', { 'fees':None, 'form':form, })
