494a7456cd9c999b3dd4f19d83b7327434f1965f
simonagrant <simon@propertymode.com.au>
2014-11-03 18:43:05 +1100

    remove data folder remove mytest folder

--- data/6.2/copy_ownership.sql
+++
@@ -1,4 +0,0 @@
-/* ONLY RUN ONCE */
-
-insert into asset_ownership(asset_property_id,  owner_citizen_id, share, i_status, date_started, date_created)
-select property_id, citizen_id, share, i_status, startdate, CURRENT_TIMESTAMP from property_ownership;
