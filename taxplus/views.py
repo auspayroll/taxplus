0239c7572918feb3a18e47c5a65d8ae7e100349f
Simon Grant <simon@propertymode.com.au>
2014-12-17 11:59:11 +1100

    remove call adjust_payments for business and property fee views.

--- taxplus/views.py
+++ taxplus/views.py
@@ -317,8 +317,6 @@ def merge_preview(request, pk):
 def property_fees(request, pk):
 	prop = get_object_or_404(Property, pk=pk)
 	fees = prop.property_fees.filter(amount__gt=0)
-	for fee in fees:
-		fee.adjust_payments()
 	return TemplateResponse(request, 'tax/tax_tax_property_fees.html', { 'property':prop, 'fees':fees })
 
 
@@ -326,8 +324,6 @@ def property_fees(request, pk):
 def business_fees(request, pk):
 	business = get_object_or_404(Business, pk=pk)
 	fees = business.business_fees.filter(amount__gt=0)
-	for fee in fees:
-		fee.adjust_payments()
 	return TemplateResponse(request, 'tax/business_fees.html', { 'business':business, 'fees':fees })
 
 
