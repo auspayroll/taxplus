f42a0874955a675c417d64832f8c7f81b73a8755
Simon Grant <simon@propertymode.com.au>
2015-04-01 18:10:26 +1100

    url pattern updates, make shorter/saner urls.

--- dev1/urls.py
+++ dev1/urls.py
@@ -10,8 +10,11 @@ urlpatterns = patterns('',
 
 	url(r'^il/(?P<key>\w+)/$', taxplus_views.mobile_invoice_landing, name='mobile_invoice_landing'),
 	url(r'^i/(?P<key>\w+)/$', taxplus_views.mobile_invoice, name='mobile_invoice'),
-	url(r'^admin/tax/tax/business/(?P<pk>\d+)/$', taxplus_views.business_fees, name='business_default'),
-	url(r'^admin/tax/tax/property/(?P<pk>\d+)/$', taxplus_views.property_fees, name='property_default'),
+	url(r'^business/(?P<pk>\d+)/$', taxplus_views.business_fees, name='business_default'),
+	url(r'^property/(?P<pk>\d+)/$', taxplus_views.property_fees, name='property_default'),
+	url(r'^admin/tax/tax/property/(?P<pk>\d+)/$', taxplus_views.property_fees, name='redirect_from_search'),
+	url(r'^admin/log/log/$', taxplus_views.request_log, name='request_log'),
+	url(r'^log/change/$', taxplus_views.change_log, name='change_log'),
 
 	url(r'^admin/property/', include('property.urls')),
 	url(r'^admin/auth/', include('pmauth.urls')),
@@ -21,8 +24,8 @@ urlpatterns = patterns('',
 	url(r'^admin/asset/business/change_business/(?P<pk>\d+)/$', taxplus_views.business_update, name='change_business_deprecated'),
 	url(r'^admin/tax/tax/business/(?P<pk>\d+)/edit_business/$', taxplus_views.business_update, name='change_business_deprecated2'),
 
-	url(r'^admin/business_update_region/(?P<pk>\d+)/$', taxplus_views.business_update_region, name='business_update_region'),
-	url(r'^admin/business_payments/(?P<pk>\d+)/$', taxplus_views.business_payments, name='business_payments'),
+	url(r'^business/(?P<pk>\d+)/update_region/$', taxplus_views.business_update_region, name='business_update_region'),
+	url(r'^business/(?P<pk>\d+)/payments/$', taxplus_views.business_payments, name='business_payments'),
 	url(r'^admin/merge_preview/(?P<pk>\d+)/$', taxplus_views.merge_preview, name='merge_preview'),
 
 	url(r'^admin/message_batches/$', taxplus_views.message_batches, name='message_batches'),
@@ -30,16 +33,16 @@ urlpatterns = patterns('',
 	url(r'^admin/batch_messages/(?P<pk>\d+)/$', taxplus_views.batch_messages, name='batch_messages'),
 	url(r'^admin/batch_messages_csv/(?P<pk>\d+)/$', taxplus_views.batch_messages, {'csv':True}, name='batch_messages_csv'),
 
-	url(r'^admin/tax/tax/payment_search/$', taxplus_views.payment_search, name='search_payments'),
+	url(r'^payment/search/$', taxplus_views.payment_search, name='search_payments'),
 	url(r'^admin/to_fee_from_payment_search/(?P<pk>\d+)/$', taxplus_views.to_fee_from_payment_search, name='to_fee_from_payment_search'),
 
-	url(r'^admin/property_fees/(?P<pk>\d+)/fees/$', taxplus_views.property_fees, name='property_fees_link'),
-	url(r'^admin/tax/tax/property/(?P<pk>\d+)/map/$', taxplus_views.property_map, name='property_map'),
+	url(r'^property/(?P<pk>\d+)/fees/$', taxplus_views.property_fees, name='property_fees_link'),
+	url(r'^property/(?P<pk>\d+)/map/$', taxplus_views.property_map, name='property_map'),
 
-	url(r'^admin/property/media/(?P<pk>\d+)/$', taxplus_views.property_media, name='property_media'),
+	url(r'^property/(?P<pk>\d+)/media/$', taxplus_views.property_media, name='property_media'),
 
-	url(r'^admin/reverse_payments/$', taxplus_views.reverse_payments, name='reverse_payments'),
-	url(r'^admin/tax/tax/payment_reverse/$', taxplus_views.payment_reverse, name='reverse_payments_redirect_deprecated'),
+	url(r'^payments/reverse/$', taxplus_views.reverse_payments, name='reverse_payments'),
+	url(r'^payments/reverse/deprecated/$', taxplus_views.payment_reverse, name='reverse_payments_redirect_deprecated'),
 
 
 	url(r'^admin/tax/', include('jtax.urls')),
@@ -53,7 +56,7 @@ urlpatterns = patterns('',
 	url(r'^admin/cleaning_audit/', taxplus_views.cleaning_audit, name='cleaning_audit'),
 	url(r'^admin/cleaning_debtors/', taxplus_views.cleaning_debtors, name='cleaning_debtors'),
 
-	url(r'^admin/property_fees/(?P<pk>\d+)/$', taxplus_views.property_fees, name='property_fees'),
+	url(r'^property/(?P<pk>\d+)/fees/$', taxplus_views.property_fees, name='property_fees'),
 	url(r'^admin/property_payments/(?P<pk>\d+)/$', taxplus_views.property_payments, name='property_payments'),
 	url(r'^admin/property_leases/(?P<pk>\d+)/$', taxplus_views.leases, name='property_leases'),
 	url(r'^admin/edit_lease/(?P<pk>\d+)/$', taxplus_views.edit_lease, name='edit_lease'),
