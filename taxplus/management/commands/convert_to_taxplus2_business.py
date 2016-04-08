df36b6259de81c5e3f7d8fcfd4d7af0efc1fa811
Simon Grant <simon@propertymode.com.au>
2016-04-08 14:16:26 +1000

    import cleaning fee script

--- taxplus/management/commands/convert_to_taxplus2_business.py
+++ taxplus/management/commands/convert_to_taxplus2_business.py
@@ -36,17 +36,18 @@ class Command(BaseCommand):
 		cleaning = CategoryChoice.objects.get(category__code='fee_type', code='cleaning')
 		for b in Business.objects.all().order_by('id'):
 			account = Account.objects.create(name=b.name, start_date=b.date_started, end_date=b.closed_date, phone=b.phone1 or b.phone2, email=b.email, business_id=b.pk)
-			print 'Business: %s' % b.pk
+			print 'Business: %s, Account %s' % (b.pk, account.pk)
 
 			if b.business_category_id:
 				sub_category = CategoryChoice.objects.get(code="cat%s" % b.business_category_id, category__code='cleaning_rate')
 			else:
-				sub_category = None
+				sub_category = CategoryChoice.objects.get(code="cat1", category__code='cleaning_rate')
 
 			af = AccountFee.objects.create(account=account, auto=True, period=12, fee_type=cleaning, fee_subtype=sub_category,
 				from_date=account.start_date, to_date=account.end_date, village=b.village, cell=b.cell, sector=b.sector)
 
 			print 'Account Fee created: %s' % af.pk
+
 
 			medias = [m for m in tpMedia.objects.filter(business__pk=b.pk)]
 			new_media  = []
@@ -68,22 +69,24 @@ class Command(BaseCommand):
 				elif o.owner_business:
 					AccountHolder.objects.create(account=account, holder=o.owner_business)
 
-				# handle know property title payments
-				payfees = PayFee.objects.filter(fee__business__pk=b.pk, fee__category__code='cleaning')
-				for payfee in payfees:
-					r = payfee.receipt
-					if r.pk not in receipts:
-						receipts.append(r.pk)
-						user = get_user(r.user)
-						deposit = BankDeposit.objects.create(account=account, bank_receipt_no=r.bank_receipt, bank=r.bank,
-							date_banked=r.paid_date, depositor_name=r.payer_name, amount=r.amount, status_id=r.status_id,
-							note=r.note, created=r.date_time, sector_receipt=r.sector_receipt, old_receipt_id=r.pk, user=user)
-					for nm in new_media:
-						if payfee.pk == nm.payfee_id or payfee.fee_id == nm.fee_id or nm.receipt_id  == r.pk:
-							if nm.receipt_id == r.pk:
-								nm.record = deposit
-							nm.save()
-							print '1111111. media pk %s saved' % nm.pk
+			# handle know property title payments
+			payfees = PayFee.objects.filter(fee__business__pk=b.pk, fee__category__code='cleaning')
+			for payfee in payfees:
+				r = payfee.receipt
+				if r.pk not in receipts:
+					receipts.append(r.pk)
+					user = get_user(r.user)
+					deposit = BankDeposit.objects.create(account=account, bank_receipt_no=r.bank_receipt, bank=r.bank,
+						date_banked=r.paid_date, depositor_name=r.payer_name, amount=r.amount, status_id=r.status_id,
+						note=r.note, created=r.date_time, sector_receipt=r.sector_receipt, old_receipt_id=r.pk, user=user)
+					print 'deposit %s' % deposit.pk
+
+				for nm in new_media:
+					if payfee.pk == nm.payfee_id or payfee.fee_id == nm.fee_id or nm.receipt_id  == r.pk:
+						if nm.receipt_id == r.pk:
+							nm.record = deposit
+						nm.save()
+						print '1111111. media pk %s saved' % nm.pk
 
 			account.transactions(update=True)
 
