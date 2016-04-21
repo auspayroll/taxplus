5fb923902d883f888d91d2346d17494446797d97
Simon Grant <simon@propertymode.com.au>
2016-04-21 10:16:01 +1000

    region report, exclude closed off fees.

--- crud/sql/region_outstanding.sql
+++ crud/sql/region_outstanding.sql
@@ -3,23 +3,23 @@ delete from crud_currentoutstanding;
 
 insert into crud_currentoutstanding(balance, overdue, fee_type_id, village_id, accounts)
 select sum(balance), sum(overdue), fee_type_id, village_id, count(*)
-from crud_accountfee
+from crud_accountfee where closed is null
 group by fee_type_id, village_id;
 
 
 insert into crud_currentoutstanding(balance, overdue, fee_type_id, cell_id, accounts)
 select sum(balance), sum(overdue), fee_type_id, cell_id, count(*)
-from crud_accountfee
+from crud_accountfee where closed is null
 group by fee_type_id, cell_id;
 
 
 insert into crud_currentoutstanding(balance, overdue, fee_type_id, sector_id, accounts)
 select sum(balance), sum(overdue), fee_type_id, sector_id, count(*)
-from crud_accountfee
+from crud_accountfee where closed is null
 group by fee_type_id, sector_id;
 
 
 insert into crud_currentoutstanding(balance, overdue, fee_type_id, district_id, accounts)
 select sum(balance), sum(overdue), fee_type_id, district_id, count(*)
-from crud_accountfee
+from crud_accountfee where closed is null
 group by fee_type_id, district_id;
