e4579a74299510d628139fd5425ffc8340713eee
Simon Grant <simon@propertymode.com.au>
2016-04-28 15:05:00 +1000

    management script to set close off date to account end date

---
+++ crud/management/commands/close_off_correction.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+import dateutil.parser
+import os
+import re
+from datetime import datetime, date, time
+from django.db import IntegrityError, transaction
+import csv
+import codecs
+from django.core.exceptions import *
+from crud.models import *
+from django.db.models import Q
+from decimal import Decimal
+from datetime import date
+from taxplus.models import Category, CategoryChoice
+from dateutil.relativedelta import relativedelta
+
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+
+class Command(BaseCommand):
+	args = 'None'
+	help = ''
+	name= 'Import Kicukiro'
+
+	# @transaction.commit_on_success
+	def handle(self, *args, **options):
+		accounts = Account.objects.filter(end_date__isnull=False, end_date__lt=F('period_ending'))
+		for account in accounts:
+			print account.pk
+			fees = account.account_fees.all()
+			if fees:
+				if fees[0].fee_type.code == 'land_lease' and account.end_date < date(2014,12,31):
+					account.close_off_period(write_off=True)
+					account.transactions(update=True)
+				elif fees[0].fee_type.code == 'cleaning' and account.end_date < date(2015,12,31):
+					account.close_off_period(write_off=True)
+					account.transactions(update=True)
